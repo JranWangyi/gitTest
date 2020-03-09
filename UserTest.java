5c95c92809b4697e49108bd0b2f948693f972cc2
JranWangyi <1758836955@qq.com>
2020-03-09 23:29:41 +0800

    提交对userTest的修改

--- UserTest.java
+++ UserTest.java
@@ -5,6 +5,7 @@ public class UserTest {
 	public static void main(String[] args) {
 		User user1 = new User(1,"张三",20);
 		User user2 = new User(2,"李四",20);
+		User user3 = new User(3,"李四",30);
 		
 		System.out.println(user1.equals(user2));
 	}
