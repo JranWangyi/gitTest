aa2bd72b9223a0fea585b0cfc20ce668b6d63c17
JranWangyi <1758836955@qq.com>
2020-03-09 23:53:47 +0800

    提交了b.txt，c.txt

--- UserTest.java
+++ UserTest.java
@@ -6,6 +6,7 @@ public class UserTest {
 		User user1 = new User(1,"张三",20);
 		User user2 = new User(2,"李四",20);
 		User user3 = new User(3,"李四",30);
+		User user4 = new User(4,"李四",40);
 		
 		System.out.println(user1.equals(user2));
 	}
